002e81521288bd903fa47d5f23cb082b6ad7570f
linuxpedro <pdro.dev97@gmail.com>
2024-10-20 21:59:37 -0300

    feat: svelte pages

---
+++ src/routes/contato/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let params: { id: string };
+  import Conversation from '$components/Conversation.svelte';
+</script>
+
+<main>
+  <h1>Conversas com {params.id}</h1>
+  <Conversation {id} />
+</main>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import LoginForm from '$components/LoginForm.svelte';
+</script>
+
+<main>
+  <h1>Login</h1>
+  <LoginForm />
+</main>
